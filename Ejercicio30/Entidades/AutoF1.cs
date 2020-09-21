866c393e6e81c143fc0b39680ea6edc79ed7351b
MaximilianoLenzuen <maximilianolenzuen@gmail.com>
2020-09-20 22:45:45 -0300

    El lenzuen mas piola

---
+++ Ejercicio30/Entidades/AutoF1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class AutoF1
+    {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;
+        public AutoF1(short numero, string escuderia)
+        {
+            this.numero = numero;
+            this.escuderia = escuderia;
+            this.enCompetencia = false;
+            this.cantidadCombustible = 0;
+        }
+
+        public short CantidadDeCombustibles
+        {
+            get
+            {
+                return this.cantidadCombustible;
+            }
+            set
+            {
+                this.cantidadCombustible = value;
+            }
+        }
+
+        public bool EnCompetencia
+        {
+            get
+            {
+                return this.enCompetencia;
+            }
+            set
+            {
+                this.enCompetencia = value;
+            }
+        }
+        public short VueltasRestantes
+        {
+            get
+            {
+                return this.vueltasRestantes;
+            }
+            set
+            {
+                this.vueltasRestantes = value;
+            }
+        }
+
+
+
+        public string MostrarDatos()
+        {
+            return $"Numero: {this.numero}, Escuderia: {this.escuderia} || Cantidad de combustible: {this.cantidadCombustible} || Vueltas restantes: {this.vueltasRestantes}";
+        }
+        
+        public static bool operator ==(AutoF1 auto1, AutoF1 auto2)
+        {
+            return (auto1.numero == auto2.numero && auto1.escuderia == auto2.escuderia);
+        }
+        public static bool operator !=(AutoF1 auto1, AutoF1 auto2)
+        {
+            return !(auto1 == auto2);
+        }
+        
+
+    }
+
+}
---
+++ Ejercicio30/Entidades/Competencia.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<AutoF1> competidores;
+
+        private Competencia()
+        {
+            this.competidores = new List<AutoF1>();
+        }
+
+        public Competencia(short cantidadVueltas, short cantidadCompetidores) : this()
+        {
+            this.cantidadVueltas = cantidadVueltas;
+            this.cantidadCompetidores = cantidadCompetidores;
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Cantidad de competidores: {0}\nCantidad de vueltas: {1}", this.cantidadCompetidores, this.cantidadVueltas);
+            foreach (AutoF1 a in this.competidores)
+            {
+                sb.AppendFormat("\nAutoF1: {0}", a.MostrarDatos());
+            }
+            return sb.ToString();
+        }
+
+        public static bool operator +(Competencia competencia, AutoF1 auto)
+        {
+            Random random = new Random();
+
+            if (competencia.competidores.Count() < 5 && competencia == auto)
+            {
+                competencia.competidores.Add(auto);
+                auto.EnCompetencia = true;
+                auto.VueltasRestantes = competencia.cantidadVueltas;
+                auto.CantidadDeCombustibles =(short)random.Next(15, 100);
+                return true;
+            }
+            return false;
+        }
+        public static bool operator -(Competencia competencia, AutoF1 auto)
+        {
+            return !(competencia == auto);
+        }
+        public static bool operator ==(Competencia competencia, AutoF1 auto)
+        {
+            foreach (AutoF1 b in competencia.competidores)
+            {
+                if (b == auto)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Competencia competencia, AutoF1 auto)
+        {
+            return !(competencia == auto);
+        }
+    }
+}
