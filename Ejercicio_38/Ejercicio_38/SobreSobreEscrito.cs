70ca591f344de1f72cfba71e0c0c514fa6375ecf
fgarancini <f.garancini15@gmail.com>
2020-10-03 00:31:11 -0300

    Ejercicio 38 - 39

---
+++ Ejercicio_38/Ejercicio_38/SobreSobreEscrito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_38
+{
+    public class SobreSobreEscrito : Sobreescrito
+    {
+        public SobreSobreEscrito():base()
+        {
+
+        }
+        public override string MiPropiedad
+        {
+            get
+            {
+                return this.miAtributo;
+            }
+        }
+
+        public override string MiMetodo()
+        {
+            return MiPropiedad;
+        }
+    }
+}
---
+++ Ejercicio_38/Ejercicio_38/Sobrescrito.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_38
+{
+    public abstract class Sobreescrito
+    {
+        protected string miAtributo;
+
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar abstractos";
+        }
+
+        public abstract string MiPropiedad 
+        {
+            get;
+        }
+
+        public abstract string MiMetodo();
+        public string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+
+        public bool Equals(Sobreescrito sobreescrito,Sobreescrito sobreescrito2)
+        {
+            if(sobreescrito is Sobreescrito && sobreescrito2 is Sobreescrito)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+
+        public int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
